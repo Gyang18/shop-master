ceb7437ec77f665f16d5e3c0572c43ebc64bc699
Gyang18 <1763645020@qq.com>
2019-04-18 00:51:27 +0800

    优化header头部ui样式

--- src/layouts/BasicLayout/components/Header.jsx
+++ src/layouts/BasicLayout/components/Header.jsx
@@ -7,15 +7,28 @@ const Header = props => {
   return (
     <Headers style={{ background: '#fff' }}>
       <Row type='flex' align='middle' justify='space-between'>
-        <Col>
+        <Col span={12}>
           <Icon className='trigger' type={props.collapsed ? 'menu-unfold' : 'menu-fold'} onClick={props.toggle} />
         </Col>
-        <Col>
-          <div className='nav-menu'>
-            <div className='nav-item'>退出登陆</div>
+        <Col offset={2} span={10}>
+          <div className='nav-menu' data-flex='main:right cross:center'>
+            <div className='nav-item icon-item'>
+              <Icon type='exclamation-circle' />
+              <span>意见反馈</span>
+            </div>
+            <div className='nav-item icon-item'>
+              <span>
+                <Icon type='question-circle' />
+                使用文档
+              </span>
+            </div>
+            <div className='nav-item icon-item'>
+              <Icon type='logout' />
+              退出登陆
+            </div>
             <div className='nav-item'>
-              <Avatar size='large'>Gyang</Avatar>
-              <span>admin</span>
+              <Avatar size='large' icon='user' />
+              <span className='name'>admin</span>
             </div>
           </div>
         </Col>
