d4ec06210d2acb5d9e6b550299ab1bf2f697602a
Gyang18 <1763645020@qq.com>
2019-04-18 00:24:26 +0800

    新增用户相关ui

--- src/layouts/BasicLayout/components/Header.jsx
+++ src/layouts/BasicLayout/components/Header.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
-import { Layout, Icon } from 'antd'
+import { Layout, Icon, Row, Col, Avatar } from 'antd'
 
 const Headers = Layout.Header
 
 const Header = props => {
   return (
     <Headers style={{ background: '#fff' }}>
-      <Icon className='trigger' type={props.collapsed ? 'menu-unfold' : 'menu-fold'} onClick={props.toggle} />
+      <Row type='flex' align='middle' justify='space-between'>
+        <Col>
+          <Icon className='trigger' type={props.collapsed ? 'menu-unfold' : 'menu-fold'} onClick={props.toggle} />
+        </Col>
+        <Col>
+          <div className='nav-menu'>
+            <div className='nav-item'>退出登陆</div>
+            <div className='nav-item'>
+              <Avatar size='large'>Gyang</Avatar>
+              <span>admin</span>
+            </div>
+          </div>
+        </Col>
+      </Row>
     </Headers>
   )
 }
