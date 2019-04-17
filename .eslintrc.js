43595ba9d45c1bb535e224300f5f456871949be4
Gyang18 <1763645020@qq.com>
2019-04-18 00:27:10 +0800

    新增eslint rule规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
     //关闭使用解构赋值的检测
     'react/destructuring-assignment': [0, 'always'],
     // 解决require报错问题
-    'import/no-extraneous-dependencies': ['error', { devDependencies: true }]
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    "react/jsx-one-expression-per-line": [0]
   }
 }
