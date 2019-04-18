6a3c08c1f2464fdef3d7bcd5c15b30148003e0d5
Gyang18 <1763645020@qq.com>
2019-04-18 15:14:07 +0800

    新增路径别名配置

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,9 @@
 /**
- * creata-react-app 自定义配置
+ * creata-react-app 自定义webpack配置
  *
  * * */
-const { override, fixBabelImports, addLessLoader } = require('customize-cra')
-
+const { override, fixBabelImports, addLessLoader, addWebpackAlias } = require('customize-cra')
+const path = require('path')
 module.exports = override(
   // 实现按需加载antd 组件与css
   fixBabelImports('import', {
@@ -11,8 +11,20 @@ module.exports = override(
     libraryDirectory: 'es',
     style: 'css'
   }),
+  // 启用less
   addLessLoader({
     strictMath: true,
     noIeCompat: true
+  }),
+  // 设置路径别名
+  addWebpackAlias({
+    '@': path.resolve(__dirname, 'src'),
+    '@assets': path.resolve(__dirname, 'src/assets'),
+    '@comp': path.resolve(__dirname, 'src/components'),
+    '@layouts': path.resolve(__dirname, 'src/layouts'),
+    '@pages': path.resolve(__dirname, 'src/pages'),
+    '@plug': path.resolve(__dirname, 'src/plugins'),
+    '@server': path.resolve(__dirname, 'src/server'),
+    '@utils': path.resolve(__dirname, 'src/utils'),
   })
 )
