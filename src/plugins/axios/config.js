b411a04ac070aedd59f547f814febab73572c8e2
Gyang18 <1763645020@qq.com>
2019-04-20 19:36:11 +0800

    更正baseURL地址

--- src/plugins/axios/config.js
+++ src/plugins/axios/config.js
@@ -8,7 +8,7 @@
  * @param {Number} maxReqNum  重发请求最大数
  * */
 export default {
-  baseURL: process.env.REACT_API_UR, // 请求地址
+  baseURL: process.env.REACT_APP_REACT_API_URL, // 请求地址
   header: { 'Content-Type': 'application/json;charset=UTF-8' },
   timeout: 600000
 }
