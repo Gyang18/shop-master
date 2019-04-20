db731a6855a6c67159ec26ba7c11cd9e9b15735b
Gyang18 <1763645020@qq.com>
2019-04-20 19:23:40 +0800

    新增用户登陆注册接口

---
+++ src/api/user.js
@@ -0,0 +1,9 @@
+/*
+ * 管理员操作相关接口
+ * */
+import req from '@plug/axios'
+
+// 用户登陆
+export const userLogin = param => req.reqPost('/admin/login', { ...param })
+// 用户注册
+export const userRegister = param => req.reqPost('/admin/register', { params: param })
