c1596c39d7bb90646197ec38d6a1bad821b28621
Gyang18 <1763645020@qq.com>
2019-05-07 22:29:08 +0800

    修改用户登陆api

--- src/api/user.js
+++ src/api/user.js
@@ -4,6 +4,6 @@
 import req from '@plug/axios'
 
 // 用户登陆
-export const userLogin = param => req.reqPost('/admin/login', { ...param })
+export const userLogin = param => req.reqPost('/user/login', { ...param })
 // 用户注册
 export const userRegister = param => req.reqPost('/admin/register', { params: param })
