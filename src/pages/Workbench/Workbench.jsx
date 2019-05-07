59e734ea6348d3bf1548ce6cefe9065c70550ae2
Gyang18 <1763645020@qq.com>
2019-05-07 22:42:47 +0800

    修改组件结构

--- src/pages/Workbench/Workbench.jsx
+++ src/pages/Workbench/Workbench.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Row, Col } from 'antd'
 
 const Workbench = () => {
   return (
-    <div className='App'>
-      <h1>工作台</h1>
+    <div className='workbench'>
+      <Row type='flex' align='middle' justify='space-between'>
+        <Col>个人信息左</Col>
+        <Col>个人信息右</Col>
+      </Row>
     </div>
   )
 }
