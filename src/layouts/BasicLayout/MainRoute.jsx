74561979ae5bccbba77030e5cfdb1610d7a32453
Gyang18 <1763645020@qq.com>
2019-05-08 22:51:52 +0800

    取消页面过渡效果

--- src/layouts/BasicLayout/MainRoute.jsx
+++ src/layouts/BasicLayout/MainRoute.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-import { Switch, Route, Redirect, withRouter } from 'react-router-dom'
-import { CSSTransition, TransitionGroup } from 'react-transition-group'
+import { Switch, Route, Redirect } from 'react-router-dom'
+// import { CSSTransition, TransitionGroup } from 'react-transition-group'
 import NotFound from '@comp/NotFound'
 import { mainRouterConfig } from '../../routerConfig'
 import '@/styles/animate.css'
@@ -14,21 +14,20 @@ class MainRoute extends Component {
   }
 
   render() {
-    const { location } = this.props
     return (
       /* 装载路由 */
-      <TransitionGroup>
-        <CSSTransition key={location.key} timeout={300} classNames='my-node'>
-          <Switch>
-            {mainRouterConfig.map(this.renderNormalRoute)}
-            {/* 根路由默认重定向到 /workbench */}
-            <Redirect exact from='/' to='/workbench' />
-            {/* 未匹配到的路由重定向到 NotFound */}
-            <Route component={NotFound} />
-          </Switch>
-        </CSSTransition>
-      </TransitionGroup>
+      /* <TransitionGroup> */
+      /*  <CSSTransition key={location.key} timeout={200} classNames='page'> */
+      /*  </CSSTransition> */
+      /* </TransitionGroup> */
+      <Switch>
+        {mainRouterConfig.map(this.renderNormalRoute)}
+        {/* 根路由默认重定向到 /workbench */}
+        <Redirect exact from='/' to='/workbench' />
+        {/* 未匹配到的路由重定向到 NotFound */}
+        <Route component={NotFound} />
+      </Switch>
     )
   }
 }
-export default withRouter(MainRoute)
+export default MainRoute
