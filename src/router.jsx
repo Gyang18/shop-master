e60634e198b3abda2dd4f4c88f312fc0b818bcff
Gyang18 <1763645020@qq.com>
2019-05-09 18:28:32 +0800

    更新国际化配置

--- src/router.jsx
+++ src/router.jsx
@@ -3,20 +3,28 @@
  * * */
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import React from 'react'
+// 中文化
+import { LocaleProvider } from 'antd'
+import zhCN from 'antd/lib/locale-provider/zh_CN'
+import moment from 'moment'
+import 'moment/locale/zh-cn'
 import BasicLayout from './layouts/BasicLayout'
 import UserLayout from './layouts/UserLayout'
 /**
  * 按照layout 对路由进行分组
  *
  * * */
+moment.locale('zh-cn')
 const router = () => {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path='/user' component={UserLayout} />
-        <Route path='/' component={BasicLayout} />
-      </Switch>
-    </BrowserRouter>
+    <LocaleProvider locale={zhCN}>
+      <BrowserRouter>
+        <Switch>
+          <Route path='/user' component={UserLayout} />
+          <Route path='/' component={BasicLayout} />
+        </Switch>
+      </BrowserRouter>
+    </LocaleProvider>
   )
 }
 
